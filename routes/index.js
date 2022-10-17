38c95a4c4b8c15575c001396e705954af6a75764
Srijit <srijit@amazon.com>
2022-10-17 17:47:38 -0400

    Tweaks and updates

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AP Hotel' });
+  res.render('index', { title: 'AWS App Runner Hotel' });
 });
 
 module.exports = router;
