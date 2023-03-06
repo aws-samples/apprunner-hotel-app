8ba04e8a380b521e7cec2b7df2024e4f3f0633f3
Noah Leuthaeuser <noaleuth@amazon.com>
2023-03-06 15:29:22 -0700

    converted rds connection to function

--- rds.js
+++ rds.js
@@ -3,37 +3,42 @@ var config = require('./config');
 var rdsPool = null;
 var rdsUrl = null;
 
-if(!config.infra.region) {
-  throw new Error('AWS_REGION environment variable must be set. This is usually set by Fargate');
-}
-console.log('Application launched in: ', config.region);
-
-if(!config.secret.db_secret) {
-  throw new Error('MYSQL_SECRET environment variable must be set');
-}
-
-const secret = JSON.parse(config.secret.db_secret);
-rdsUrl = secret.host;
-console.log(`Connecting to RDS at ${rdsUrl}`);
-
-rdsPool = mysql.createPool({
-  connectionLimit : 12,
-  host: secret.host,
-  password: secret.password,
-  user: secret.username
-});
-rdsPool.on('error', err=> {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
-}); 
-
-rdsPool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-   if (error) {
-    console.log(error);
+function rds() {
+  if(!config.infra.region) {
+    throw new Error('AWS_REGION environment variable must be set. This is usually set by Fargate');
   }
-   else {
-   console.log('The solution is: ', results[0].solution);
+  console.log('Application launched in: ', config.infra.region);
+  
+  if(!config.secret.db_secret) {
+    throw new Error('MYSQL_SECRET environment variable must be set');
   }
- });
+  
+  const secret = JSON.parse(config.secret.db_secret);
+  rdsUrl = secret.host;
+  console.log(`Connecting to RDS at ${rdsUrl}`);
+  
+  rdsPool = mysql.createPool({
+    connectionLimit : 12,
+    host: secret.host,
+    password: secret.password,
+    user: secret.username
+  });
+  rdsPool.on('error', err=> {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+  }); 
+  
+  rdsPool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+     if (error) {
+      console.log(error);
+    }
+     else {
+     console.log('The solution is: ', results[0].solution);
+    }
+   });
+  
+  return [rdsPool, rdsUrl];
+}
 
-module.exports = function() { return [rdsPool, rdsUrl]; }
+
+module.exports = rds;
