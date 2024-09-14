9bb4b2dfd05c05362ce25032e6f95a80de285030
Sam Kool <samkool@amazon.com>
2024-09-14 15:01:18 +0200

    Update secret retrieval

--- config.js
+++ config.js
@@ -6,4 +6,14 @@ var config = {
 config.infra.region = process.env.AWS_REGION;
 config.app.hotel_name = process.env.HOTEL_NAME;
 config.secret.db_secret = process.env.MYSQL_SECRET;
+config.secret.db_secret_value = retrieve_secret_value(config.secret.db_secret);
+
+function retrieve_secret_value(secret_id) {
+    var AWS = require('aws-sdk');
+    var client = new AWS.SecretsManager({
+        region: config.infra.region
+    });
+    return client.getSecretValue({SecretId: secret_id}).promise();
+}
+
 module.exports = config;
